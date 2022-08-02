97b8feb1358812a896362c810cf269691e0d3488
AbuBakrKhan24 <abubakrkhan224@gmail.com>
2022-08-02 16:35:22 +0200

    changes

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ app.listen(app.get("port"), () => {
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.sendFile(_dirname + "/" + "index.html");
+  res.sendFile(_dirname + "/" + "Index.html");
 });
